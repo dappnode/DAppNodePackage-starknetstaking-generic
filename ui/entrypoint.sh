0cda158cdfaf0fcb5ecd93b4ca2324bc8a721493
hcastc00 <hcastc00@estudiantes.unileon.es>
2026-02-12 13:00:43 +0100

    fix: PROVIDER_HTTP_URL redundant var

--- ui/entrypoint.sh
+++ ui/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Substitute environment variables in nginx config
-envsubst '${RPC_TARGET}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${PROVIDER_HTTP_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Execute the CMD
 exec "$@"
